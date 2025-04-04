3f667ed8528437b7f74b5d2f9ad4c4c1690060a4
smjest0802 <smjest0802@gmail.com>
2025-04-04 16:38:22 -0400

    Add reporting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from stats import count_words
+from stats import count_letters
+from stats import sort_letters
 
 def get_book_text(filepath):
     contents = ""
@@ -9,8 +11,19 @@ def get_book_text(filepath):
     return contents
 
 def main():
-    file_contents = get_book_text("./books/frankenstein.txt")
+    filename = "books/frankenstein.txt"
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filename}")
+    file_contents = get_book_text(filename)
     word_count = count_words(file_contents)
-    print (f"{word_count} words found in the document")
+    print ("----------- Word Count ----------")
+    print (f"Found {word_count} total words")
+    letter_count = count_letters(file_contents)
+    sorted_letters = sort_letters(letter_count)
+    print ("--------- Character Count -------")
+    for entry in sorted_letters:
+        if entry["letter"].isalpha():
+            print (f"{entry["letter"]}: {entry["count"]}")
+    print ("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,29 @@
 def count_words(file_contents):
     file_split = file_contents.split()
     return len(file_split)
+
+def count_letters(file_contents):
+    letter_counts = {}
+    lower_case = file_contents.lower()
+    for character in lower_case:
+        if character in letter_counts:
+            letter_counts[character] += 1
+        else:
+            letter_counts[character] = 1
+    return letter_counts
+
+def sort_letters(letter_counts):
+    sorted_letters = []
+
+    for letter in letter_counts:
+        sorted_letters.append({
+            "letter": letter,
+            "count": letter_counts[letter]
+        })
+
+    sorted_letters.sort(key=which_field, reverse=True)
+
+    return sorted_letters
+
+def which_field(entry):
+    return entry['count']
